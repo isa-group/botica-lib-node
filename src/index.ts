aa962a32ad4b1186c3341fc15e8ff5817fc9fd65
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2024-09-03 12:23:55 +0200

    feat: create SHARED_DIRECTORY constant

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { botica as default, Bot } from "./bot/index.js";
 
+export const SHARED_DIRECTORY = "/shared";
+
 export { OrderListener, PacketListener } from "./client/BoticaClient.js";
 export * from "./protocol/Packet.js";
