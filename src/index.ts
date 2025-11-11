f1d9a61fa257fd35da7022b39656940d3db55f44
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2025-11-11 11:21:53 +0100

    fix: export OrderListener as type

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export const CONTAINER_PREFIX = "botica-";
 export const SHARED_DIRECTORY = "/shared/";
 
-export { OrderListener } from "./protocol/index.js";
 export * from "@/bot/index.js";
 export * from "@/configuration/index.js";
+export type { OrderListener } from "@/protocol/index.js";
 
 export { botica as default } from "@/bot/index.js";
